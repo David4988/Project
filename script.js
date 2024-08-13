0eaede54272a2af4603004beca12a9b3b1cba5e6
David4988 <davidson4988@gmail.com>
2024-08-13 20:05:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+console.log("Double-tap to edit.");
 
-function addTask(){
-    if(inputBox.value === ''){ //checks if string is empty
+function addTask() {
+    if (inputBox.value === '') {
         alert("You must write something!");
-    }else{
-        let li = document.createElement("li"); //creates HTML tag 'li'
-        li.innerHTML = inputBox.value;         //all the text, input box recieves will go to 'li' tag
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
@@ -16,22 +18,56 @@ function addTask(){
     saveData();
 }
 
-listContainer.addEventListener("click", function(event){
-    if(event.target.tagName === "LI"){
+listContainer.addEventListener("click", function(event) {
+    if (event.target.tagName === "LI") {
         event.target.classList.toggle("checked");
         saveData();
-    }
-    else if(event.target.tagName === "SPAN"){
+    } else if (event.target.tagName === "SPAN") {
         event.target.parentElement.remove();
         saveData();
     }
-},false);
+}, false);
 
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML); //stores the data in 'li' into 'data'
+listContainer.addEventListener("dblclick", function(event) {
+    if (event.target.tagName === "LI") {
+        let li = event.target;
+        let originalText = li.innerText.replace('\u00d7', '').trim(); // Remove the delete button text before editing
+
+        let input = document.createElement("input");
+        input.type = "text";
+        input.value = originalText;
+        input.className = "edit-input";
+        li.innerHTML = "";
+        li.appendChild(input);
+        input.focus();
+
+        input.addEventListener("blur", function() {
+            saveEdit(li, input.value);
+        });
+
+        input.addEventListener("keypress", function(event) {
+            if (event.key === "Enter") {
+                saveEdit(li, input.value);
+            }
+        });
+    }
+});
+
+function saveEdit(li, newText) {
+    li.innerHTML = newText;
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    saveData();
 }
 
-function showTask(){
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
+
 showTask();
+
